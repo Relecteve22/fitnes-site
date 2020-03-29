be857142dd99ca014d666792658d94ff98d4dc9c
relecteve <Filonovaval@gmail.com>
2020-03-29 21:24:29 +0300

    Добавляет блоки

--- source/js/slider-trainers.js
+++ source/js/slider-trainers.js
@@ -3,6 +3,22 @@ $(document).ready(function () {
   $('.list-teachers').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
